0cff256dd2cabbacf1146a6f590aad0dcc57569a
Bofh82 <rodionov_s@inbox.ru>
2022-11-15 14:17:44 +0300

    Добавлена переменная DATASOURCE_PORT

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,3 +1,3 @@
-./mvnw -DDATASOURCE_HOST=192.168.137.215 clean package
+./mvnw "-DDATASOURCE_HOST=db.bromen.fun -DDATASOURCE_PORT=5432" clean package
 docker build . -t devlifestartup/cats-api:1.0.0
 docker push devlifestartup/cats-api:1.0.0
