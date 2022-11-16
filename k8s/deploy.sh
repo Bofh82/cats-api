b1b6d664fb65dd84471bd296ddd5b62425432ce5
Bofh82 <rodionov_s@inbox.ru>
2022-11-16 17:23:26 +0300

    Исправлен deploy.sh

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -2,9 +2,10 @@
 
 cat ./k8s/deployment.yaml |
 sed -r "s/ENV_JOB_BASE_NAME/$1/g" |
-sed -r "s/ENV_BUILD_NUMBER/$2/g;" |
-sed -r "s/ENV_DATASOURCE_HOST/$3/g;" |
-sed -r "s/ENV_DATASOURCE_PORT/\"$4\"/g;" |
-sed -r "s/ENV_SERVICE_NAME/$5/g;" |
-sed -r "s/ENV_SERVICE_PORT/\"$6\"/g;" |
+sed -r "s/ENV_VERSION/$2/g;" |
+sed -r "s/ENV_BUILD_NUMBER/$3/g;" |
+sed -r "s/ENV_DATASOURCE_HOST/$4/g;" |
+sed -r "s/ENV_DATASOURCE_PORT/\"$5\"/g;" |
+sed -r "s/ENV_SERVICE_NAME/$6/g;" |
+sed -r "s/ENV_SERVICE_PORT/\"$7\"/g;" |
 kubectl apply -f -
