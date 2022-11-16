8fe9cc5ff04970b20a19c966f69bb9368c0d1495
Bofh82 <rodionov_s@inbox.ru>
2022-11-16 19:15:01 +0300

    Удалил файл build_and_push.sh

--- build_and_push.sh
+++
@@ -1,3 +0,0 @@
-./mvnw "-DDATASOURCE_HOST=db.bromen.fun -DDATASOURCE_PORT=5432 -DSERVICE_NAME=cats -DSERVICE_PORT=8088" clean package
-docker build . -t devlifestartup/cats-api:1.0.0.36
-docker push devlifestartup/cats-api:1.0.0.36
