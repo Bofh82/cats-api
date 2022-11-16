6d71bcf63dc515c7c2625266f4e4fd3447833e11
Bofh82 <rodionov_s@inbox.ru>
2022-11-16 16:59:29 +0300

    Добавлен deploy.sh

---
+++ k8s/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat ./deployment.yaml |
+sed -r "s/ENV_JOB_BASE_NAME/$1/g" |
+sed -r "s/ENV_BUILD_NUMBER/$2/g;" |
+sed -r "s/ENV_DATASOURCE_HOST/$3/g;" |
+sed -r "s/ENV_DATASOURCE_PORT/\"$4\"/g;" |
+sed -r "s/ENV_SERVICE_NAME/$5/g;" |
+sed -r "s/ENV_SERVICE_PORT/\"$6\"/g;" |
+kubectl apply -f -
