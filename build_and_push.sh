af9de690c47f85e2ae70561ee49f16d18df18411
Bofh82 <rodionov_s@inbox.ru>
2022-11-16 19:12:27 +0300

    Исправил имя сервиса для k8s

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,3 +1,3 @@
-./mvnw "-DDATASOURCE_HOST=db.bromen.fun -DDATASOURCE_PORT=5432" clean package
-docker build . -t devlifestartup/cats-api:1.0.0
-docker push devlifestartup/cats-api:1.0.0
+./mvnw "-DDATASOURCE_HOST=db.bromen.fun -DDATASOURCE_PORT=5432 -DSERVICE_NAME=cats -DSERVICE_PORT=8088" clean package
+docker build . -t devlifestartup/cats-api:1.0.0.36
+docker push devlifestartup/cats-api:1.0.0.36
