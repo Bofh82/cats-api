51e1cd2611a42040929ed9488256740e3a818984
Bofh82 <rodionov_s@inbox.ru>
2022-11-18 00:04:46 +0300

    Изменил файл check_pods.sh

--- k8s/check_pods.sh
+++ k8s/check_pods.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-sleep 120s
+sleep 60s
 
 result=$(kubectl wait deployment -n default $1 --for=condition=Ready --timeout=90s 2>&1 >/dev/null)
 #result=$(kubectl rollout status deployment $1 -n default --timeout=90s 2>&1 >/dev/null)
-
-echo "Check result: $result"
 
 if echo $result | grep -q 'error'; then
     echo "Check result: $result"
