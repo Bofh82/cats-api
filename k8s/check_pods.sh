52042cfdf303158db529517241357fb8c229bbab
Bofh82 <rodionov_s@inbox.ru>
2022-11-17 20:26:40 +0300

    Изменил файл check_pods.sh

--- k8s/check_pods.sh
+++ k8s/check_pods.sh
@@ -3,12 +3,12 @@
 # wait for completion as background process - capture PID
 #kubectl wait pods -l app=$1 --for=condition=Ready --timeout=60s & completion_pid=$!
 #kubectl wait deployment/$1 --for=condition=Ready --timeout=60s & completion_pid=$!
-kubectl wait deployment -n default $1 --for condition=Available=True --timeout=60s
+kubectl wait deployment -n default $1 --for condition=Available=True --timeout=60s & completion_pid=$!
 
 # wait for failure as background process - capture PID
 #kubectl wait pods -l app=$1 --for=condition=Ready=false --timeout=60s && exit 1 & failure_pid=$!
 #kubectl wait deployment/$1 --for=condition=Ready --timeout=60s && exit 1 & failure_pid=$!
-kubectl wait deployment -n default $1 --for condition=Available=False --timeout=60s
+kubectl wait deployment -n default $1 --for condition=Available=False --timeout=60s && exit 1 & failure_pid=$!
 
 # capture exit code of the first subprocess to exit
 wait -n $completion_pid $failure_pid
