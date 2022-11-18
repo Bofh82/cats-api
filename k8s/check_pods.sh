c0f04e7e5252a3f207f17ad14b972d158a336187
Bofh82 <rodionov_s@inbox.ru>
2022-11-18 15:32:39 +0300

    Изменил файл check_pods.sh

--- k8s/check_pods.sh
+++ k8s/check_pods.sh
@@ -2,10 +2,10 @@
 
 sleep $1
 
-result=$(kubectl wait deployment -n default $2 --for condition=Available=True --timeout=$3 2>&1 >/dev/null)
+result=$(kubectl wait deployment -n default $2 --for condition=Available=True --timeout=$3 >&1 >/dev/null)
 echo "Check 1 result: $result"
 
-result=$(kubectl wait deployment -n default $2 --for condition=Available=False --timeout=$3 2>&1 >/dev/null)
+result=$(kubectl wait deployment -n default $2 --for condition=Available=False --timeout=$3 >&1 >/dev/null)
 echo "Check 2 result: $result"
 
 if echo $result | grep -q 'error'; then
