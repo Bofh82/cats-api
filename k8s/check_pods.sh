fa36fad6986be226ab32286f9cb4b0430de7b4fb
Bofh82 <rodionov_s@inbox.ru>
2022-11-18 15:49:28 +0300

    Изменил файл check_pods.sh

--- k8s/check_pods.sh
+++ k8s/check_pods.sh
@@ -2,20 +2,38 @@
 
 sleep $1
 
-result=$(kubectl wait deployment -n default $2 --for condition=Available=True --timeout=$3 >&1 >/dev/null)
-echo "Check 1 result: $result"
+# wait for completion as background process - capture PID
+kubectl wait deployment -n default $2 --for condition=Available=True --timeout=$3 & completion_pid=$!
 
-result=$(kubectl wait deployment -n default $2 --for condition=Available=False --timeout=$3 >&1 >/dev/null)
-echo "Check 2 result: $result"
+# wait for failure as background process - capture PID
+kubectl wait deployment -n default $2 --for condition=Available=False --timeout=$3 && exit 1 & failure_pid=$!
 
-if echo $result | grep -q 'error'; then
-    echo "Check result: $result"
-    exit_code=1
-    echo "Build failed with exit code ${exit_code}, exiting..."
-    #kubectl logs -l app=$2 --tail=100
+# capture exit code of the first subprocess to exit
+wait -n $completion_pid $failure_pid
+
+# store exit code in variable
+exit_code=$?
+if (( $exit_code == 0 )); then
+  echo "Job completed"
 else
-    echo "Build completed"
-    exit_code=0
+  echo "Job failed with exit code ${exit_code}, exiting..."
 fi
 exit $exit_code
+
+#result=$(kubectl wait deployment -n default $2 --for condition=Available=True --timeout=$3 2>&1 >/dev/null)
+#echo "Check 1 result: $result"
+#
+#result=$(kubectl wait deployment -n default $2 --for condition=Available=False --timeout=$3 2>&1 >/dev/null)
+#echo "Check 2 result: $result"
+#
+#if echo $result | grep -q 'error'; then
+#    echo "Check result: $result"
+#    exit_code=1
+#    echo "Build failed with exit code ${exit_code}, exiting..."
+#    #kubectl logs -l app=$2 --tail=100
+#else
+#    echo "Build completed"
+#    exit_code=0
+#fi
+#exit $exit_code
 
