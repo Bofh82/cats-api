6df615240d2d4524f5725fc1d8b7613b8a2de0b0
Bofh82 <rodionov_s@inbox.ru>
2022-11-18 16:00:07 +0300

    Изменил файл check_pods.sh

--- k8s/check_pods.sh
+++ k8s/check_pods.sh
@@ -14,27 +14,9 @@ wait -n $completion_pid $failure_pid
 # store exit code in variable
 exit_code=$?
 if (( $exit_code == 0 )); then
-  echo "Job completed"
+  echo "Build completed"
 else
   kubectl logs -l app=$2 --tail=100
-  echo "Job failed with exit code ${exit_code}, exiting..."
+  echo "Build failed with exit code ${exit_code}, exiting..."
 fi
 exit $exit_code
-
-#result=$(kubectl wait deployment -n default $2 --for condition=Available=True --timeout=$3 2>&1 >/dev/null)
-#echo "Check 1 result: $result"
-#
-#result=$(kubectl wait deployment -n default $2 --for condition=Available=False --timeout=$3 2>&1 >/dev/null)
-#echo "Check 2 result: $result"
-#
-#if echo $result | grep -q 'error'; then
-#    echo "Check result: $result"
-#    exit_code=1
-#    echo "Build failed with exit code ${exit_code}, exiting..."
-#    #kubectl logs -l app=$2 --tail=100
-#else
-#    echo "Build completed"
-#    exit_code=0
-#fi
-#exit $exit_code
-
